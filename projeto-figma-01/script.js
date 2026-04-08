86266a1148f14ba3d2dd76afe5448aefbf90ee72
Gabriel Costa <gabrielcostaflu87@gmail.com>
2026-04-07 21:22:03 -0300

    Adiciona interatividade ao header e cria uma nova página de teste para navegação

--- projeto-figma-01/script.js
+++ projeto-figma-01/script.js
@@ -1,1 +1,16 @@
-console.log("Fluminense Football Club!");
+const btnLogin = document.getElementById('btn-login');
+const btnCriarConta = document.getElementById('btn-criar-conta');
+const btnHeroSection = document.getElementById('btn-hero-section');
+
+btnLogin.addEventListener('click', e => {
+    alert("Clicou btn-login");
+});
+
+btnCriarConta.addEventListener('click', e => {
+    alert("Clicou btn-criar-conta");
+});
+
+btnHeroSection.addEventListener('click', e => {
+    alert("Clicou btn-hero-section");
+    window.location.href = "pages/page1.html";
+});
